4be9027c69f1b75806a75227d97c8417438cd853
jjb8966 <jjb8966@naver.com>
2022-07-07 11:55:50 +0900

    feat: 트랜잭션 템플릿, AOP 테스트

---
+++ db-access/src/test/java/spring/dbaccess/service/MemberServiceV3_2Test.java
@@ -0,0 +1,87 @@
+package spring.dbaccess.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import spring.dbaccess.domain.Member;
+import spring.dbaccess.repository.MemberRepositoryV3;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static spring.dbaccess.connection.DBConnectionUtil.*;
+
+@Slf4j
+class MemberServiceV3_2Test {
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV3 memberRepository;
+    private MemberServiceV3_2 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USER, PW);
+        memberRepository = new MemberRepositoryV3(dataSource);
+        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+        memberService = new MemberServiceV3_2(transactionManager, memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        // A->B 2000원 계좌이체
+        log.info("=================커넥션 시작=================");
+        // 하나의 커넥션으로 동작함
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        log.info("=================커넥션 종료=================");
+
+        // then
+        Member findA = memberRepository.findById(memberA.getMemberId());
+        Member findB = memberRepository.findById(memberB.getMemberId());
+        assertThat(findA.getMoney()).isEqualTo(8000);
+        assertThat(findB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        // when
+        // A->B 2000원 계좌이체 => 실패!
+        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        // then
+        Member findA = memberRepository.findById(memberA.getMemberId());
+        Member findEx = memberRepository.findById(memberEx.getMemberId());
+        assertThat(findA.getMoney()).isEqualTo(10000);  // 예외 발생 시 트랜잭션 롤백 -> 값이 변하지 않음
+        assertThat(findEx.getMoney()).isEqualTo(10000);
+    }
+}
---
+++ db-access/src/test/java/spring/dbaccess/service/MemberServiceV3_4Test.java
@@ -0,0 +1,115 @@
+package spring.dbaccess.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import spring.dbaccess.domain.Member;
+import spring.dbaccess.repository.MemberRepositoryV3;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * 트랜잭션 - DataSource, TransactionManager 빈 자동 등록
+ */
+
+@Slf4j
+@SpringBootTest // 스프링 컨테이너 사용
+class MemberServiceV3_4Test {
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    @Autowired
+    private MemberRepositoryV3 memberRepository;
+    @Autowired
+    private MemberServiceV3_3 memberService;
+
+    @TestConfiguration
+    static class testConfig {
+
+        private final DataSource dataSource;
+
+        // 스프링 컨테이너에 자동 등록된 DataSource를 주입받음
+        public testConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource);
+        }
+
+        @Bean
+        MemberServiceV3_3 memberServiceV3_3() {
+            return new MemberServiceV3_3(memberRepositoryV3());
+        }
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        // A->B 2000원 계좌이체
+        log.info("=================커넥션 시작=================");
+        // 하나의 커넥션으로 동작함
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        log.info("=================커넥션 종료=================");
+
+        // then
+        Member findA = memberRepository.findById(memberA.getMemberId());
+        Member findB = memberRepository.findById(memberB.getMemberId());
+        assertThat(findA.getMoney()).isEqualTo(8000);
+        assertThat(findB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        // when
+        // A->B 2000원 계좌이체 => 실패!
+        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000)).isInstanceOf(IllegalStateException.class);
+
+        // then
+        Member findA = memberRepository.findById(memberA.getMemberId());
+        Member findEx = memberRepository.findById(memberEx.getMemberId());
+        assertThat(findA.getMoney()).isEqualTo(10000);  // 예외 발생 시 트랜잭션 롤백 -> 값이 변하지 않음
+        assertThat(findEx.getMoney()).isEqualTo(10000);
+    }
+
+    @Test
+    void AopCheck() {
+        log.info("memberService class={}", memberService.getClass());       // 프록시 클래스
+        log.info("memberRepository class={}", memberRepository.getClass()); // 원래 클래스
+        assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+        assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
+    }
+}
