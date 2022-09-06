f0d8adae086e06fd7e2041d046077e7cef1fb67f
jjb8966 <jjb8966@naver.com>
2022-09-06 22:39:52 +0900

    스프링 AOP - 실전 예제 @Trace, @Retry 어노테이션 만들어서 AOP 적용하기

---
+++ spring-advanced/aop/src/test/java/hello/aop/exam/ExamServiceTest.java
@@ -0,0 +1,25 @@
+package hello.aop.exam;
+
+import hello.aop.exam.aop.RetryAspect;
+import hello.aop.exam.aop.TraceAspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Import({TraceAspect.class, RetryAspect.class})
+class ExamServiceTest {
+
+    @Autowired
+    ExamService examService;
+
+    @Test
+    void test() {
+        for (int i = 0; i < 5; i++) {
+            examService.request("data" + i);
+        }
+    }
+}
